2c02a44ad0dfdd5119d666aa9aed58c4d35a2f0d
akshat <akshatbhargavadeepa@gmail.com>
2019-03-01 23:02:58 +0530

    added input component

---
+++ src/components/Input.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Input = styled.input`
+  font-size: 22px;
+  padding: 14px;
+  margin: 6px 0px;
+  outline: none;
+  border: 1px solid grey;
+  border-radius: 4px;
+  width: 54%;
+  transition: all 0.3s;
+  &:focus {
+    box-shadow: 1px 1px 1px 1px grey;
+  }
+`;
+
+export default (props) => (
+  <Input {...props} />
+);
